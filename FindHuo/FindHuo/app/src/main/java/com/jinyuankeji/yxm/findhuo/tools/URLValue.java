0376fdc6df328a51e74bfee99c7bfde72c10f620
yxiaomin <1179719147@qq.com>
2017-02-15 11:02:49 +0800

    buxiaode

---
+++ FindHuo/FindHuo/app/src/main/java/com/jinyuankeji/yxm/findhuo/tools/URLValue.java
@@ -0,0 +1,52 @@
+package com.jinyuankeji.yxm.findhuo.tools;
+
+/**
+ * Created by  yxiaomin on 2017/1/10 0010.
+ */
+
+public class URLValue {
+
+    public static String URL_NOR = "http://zhaohuo.jinyuankeji.net/api.php/App/";
+    public static String URL_LOTTERY_BANNER = "sliderList";
+    //主页
+    public static String URL_HOMEPAGE = "homePage";
+    public static String URL_FINDWORK_FINDWORK = "iconList/";
+    //彩票站更多
+    public static String URL_LOTTERY_MORE = "lotteryList";
+    //彩票站详情
+    public static String URL_LOTTERY_DEYAIL = "lotteryInfo";
+    //搜索彩票站
+    public static String URL_LOTTERY_SEARCH = "lotteryFind";
+    //找活主页
+    public static String URL_FINDWORK = "dojobPage";
+    //搜索找车
+    public static String URL_SEARCH_FINDCAR = "carFind";
+    //搜索心理师
+    public static String URL_SEARCH_PSYCHOLOGIST= "psychologistFind";
+    //找车的列表
+    public static String URL_FINDCAR_LIST= "findcarList";
+    //技工等列表
+    public static String URL_INDROCE_SOON_LIST= "introduceList";
+    //心理师列表
+    public static String URL_PSYCHOLOGIST_LIST= "psychologistList";
+    //心理师和找车页的轮播图
+    public static String URL_PSYCHOLOGIST_AND_FINGCAR_BANNER= "sliderList";
+    //零工和找车详情
+    public static String URL_LINGGONG_AND_FINDCAR_DETAIL= "introduceInfo";
+    //心理咨询详情
+    public static String URL_PSYCHOLOGIST_DETAIL= "psychologistInfo";
+    //找车列表判断是否是司机
+    public static String URL_DRIVER_OR_NOT= "driverJump";
+    //判断心理师入驻是否跳页
+    public static String URL_PSYCHOLOGIST_OR_NOT= "psychologistJump";
+    //彩票站付款页、余额支付
+    public static String URL_LOTTERY_PAY_CREATE= "orderCreate";
+    //搜索技工
+    public static String URL_FINDWORK_SEARCH_JIGONG= "introduceFind";
+
+
+
+
+
+
+}
