7b4864911eaeb8b143022cb5091003d8dba45e35
yxiaomin <1179719147@qq.com>
2017-02-27 16:12:22 +0800

    ppppp

--- FindHuo/app/src/main/java/com/jinyuankeji/yxm/findhuo/tools/URLValue.java
+++ FindHuo/app/src/main/java/com/jinyuankeji/yxm/findhuo/tools/URLValue.java
@@ -45,6 +45,52 @@ public class URLValue {
     public static String URL_FINDWORK_SEARCH_JIGONG= "introduceFind";
 
 
+    //短信验证
+    public static String checkPhone= "checkPhone";
+    //忘记密码
+    public static String forgetPwd= "forgetPwd";
+    //注册
+    public static String appRegist= "appRegist";
+    //登录
+    public static String appLogin= "appLogin";
+    //我的界面、个人信息读取、消息提醒读取、账户余额
+    public static String userInfo= "userInfo";
+    //消息通知
+    public static String noticeList= "noticeList";
+    //余额——充值记录
+    public static String rechargeList= "rechargeList";
+    //消费记录
+    public static String orderList= "orderList";
+    //我的发布列表
+    public static String myintroduceList= "myintroduceList";
+    //我的发布列表删除
+    public static String introduceDel= "introduceDel";
+    //所有上传图片
+    public static String uploadPicture= "uploadPicture";
+    //我的界面——》修改个人信息和修改彩票提醒
+    public static String userModify= "userModify";
+    //我的界面--》我购买的彩票——》待出票和已出票
+    public static String noDraft= "noDraft";
+    //我的界面——》已出票列表-》已出票详情
+    public static String orderInfo= "orderInfo";
+    //发布——》发布我要找车
+    public static String taxiFind= "taxiFind";
+    //发布——》发布我要找零工
+    public static String workerFind= "workerFind";
+    //发布——》发布我要干零活
+    public static String doWork= "doWork";
+    //发布——》心理师入驻
+    public static String psychologist= "psychologist";
+    //我的界面——》安全设置--》修改登录密码
+    public static String pwdModify= "pwdModify";
+    //我的界面——》安全设置--》支付密码修改的身份验证
+    public static String authentication= "authentication";
+    //我的界面——》安全设置--》关于我们、用户协议
+    public static String configInfo= "configInfo";
+    //司机提交审核
+    public static String driver= "driver";
+
+
 
 
 
